51e262fd31e9eaf5b388ebbf259fb39493f6a5d2
Ecoswaech <ecoswaech@gmail.com>
2026-05-26 23:35:39 +0100

    version 1 prototype commit ....

--- proxy.ts
+++ proxy.ts
@@ -15,7 +15,7 @@ async function verifyToken(token: string) {
 const PROTECTED_ROUTES = ["/dashboard", "/passport"];
 const AUTH_ROUTES      = ["/login", "/register"];
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   const token   = req.cookies.get("swaech-token")?.value;
